762f069153af9f3ff1fee5fca2188d586116a917
Maria Vasquez <maria.ines.vf@gmail.com>
2020-09-15 13:38:54 -0600

    consultas de primer grado done

--- proyectoMain.py
+++ proyectoMain.py
@@ -15,8 +15,40 @@ def conexionesPais(tx, pais):
             cont = cont + 1
             print (str(cont) + ". " + pais["p2.nombre"])
 
+#hotel de pais
+def hotelPais(tx, pais):
+    print("Hotel de " + pais + "\n")
+    cont = 0
+    for pais in tx.run("MATCH (p:Pais {nombre: $pais})-[:HOSPEDAJE]->(h:Hotel) RETURN h.nombre",
+           pais=pais):
+            cont = cont + 1
+            print (str(cont) + ". " + pais["h.nombre"])
+#restaurante de pais
+def restaurantePais(tx, pais):
+    print("Restaurante de " + pais + "\n")
+    cont = 0
+    for pais in tx.run("MATCH (p:Pais {nombre: $pais})-[:ALIMENTACION]->(r:Restaurante) RETURN r.nombre",
+           pais=pais):
+            cont = cont + 1
+            print (str(cont) + ". " + pais["r.nombre"])
+
+#atraccion de pais
+def atraccionPais(tx, pais):
+    print("Atracción de " + pais + "\n")
+    cont = 0
+    for pais in tx.run("MATCH (p:Pais {nombre: $pais})-[:ATRACCION]->(t:LugarTurisco) RETURN t.nombre",
+           pais=pais):
+            cont = cont + 1
+            print (str(cont) + ". " + pais["t.nombre"])
+
 #print(conexionesPais("Guatemala"))
 print("conecto")
 with driver.session() as session:
 	session.read_transaction(conexionesPais, "Guatemala")
+	print("--------------------------------------------------------")
+	session.read_transaction(hotelPais, "Guatemala")
+	print("--------------------------------------------------------")
+	session.read_transaction(restaurantePais, "Guatemala")
+	print("--------------------------------------------------------")
+	session.read_transaction(atraccionPais, "Guatemala")
 	print("--------------------------------------------------------")
